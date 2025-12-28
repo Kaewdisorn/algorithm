e49e23d8fbcf546345baa622f12f11f8e46c35aa
Nane <kds.napha@gmail.com>
2025-12-28 21:10:53 +0900

    added typescript version

---
+++ easy/Two_Sum/twosum.ts
@@ -0,0 +1,42 @@
+function main(): void {
+    // Data 1
+    const nums1: number[] = [2, 7, 11, 15];
+    const target1: number = 9;
+    console.log('Data 1:');
+    console.log(`Input: nums = [${nums1}], target = ${target1}`);
+    console.log(`Output: [${twoSum(nums1, target1)}]`);
+    console.log('');
+
+    // Data 2
+    const nums2: number[] = [3, 2, 4];
+    const target2: number = 6;
+    console.log('Data 2:');
+    console.log(`Input: nums = [${nums2}], target = ${target2}`);
+    console.log(`Output: [${twoSum(nums2, target2)}]`);
+    console.log('');
+
+    // Data 3
+    const nums3: number[] = [3, 3];
+    const target3: number = 6;
+    console.log('Data 3:');
+    console.log(`Input: nums = [${nums3}], target = ${target3}`);
+    console.log(`Output: [${twoSum(nums3, target3)}]`);
+}
+
+function twoSum(nums: number[], target: number): number[] {
+    const numMap: Map<number, number> = new Map(); // value -> index
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement: number = target - nums[i];
+
+        if (numMap.has(complement)) {
+            return [numMap.get(complement)!, i];
+        }
+
+        numMap.set(nums[i], i);
+    }
+
+    return [];
+}
+
+main();
